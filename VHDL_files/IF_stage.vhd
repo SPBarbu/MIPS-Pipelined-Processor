d2f56d12756aead212e2757aaf343a12687432da
SPBarbu <stefpb0@live.ca>
2021-03-29 01:23:37 -0400

    IF start with 0 on first rising edge

--- VHDL_files/IF_stage.vhd
+++ VHDL_files/IF_stage.vhd
@@ -12,7 +12,7 @@ entity IF_stage is
 end IF_stage;
 
 architecture behavior of IF_stage is
-    signal count : integer range 0 to 63 := 0;
+    signal count : integer range 0 to 63;
 begin
     --set the upcode with the random data
     instruction_data <= std_logic_vector(to_unsigned(count, 6)) & "00000000000000000000000000";
@@ -23,8 +23,10 @@ begin
             report "IF instruction: " & integer'image(count);
             if count = 63 then
                 count <= 0;
-            else
+            elsif count >= 0 then
                 count <= count + 1;
+            else
+                count <= 0;
             end if;
         end if;
     end process;
